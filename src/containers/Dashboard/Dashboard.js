c13cabf8921686354ce4980f4b292d37c8bebbc7
Nikola Stojanovic <nikstojanovic84@gmail.com>
2021-08-21 14:38:53 +0200

    component and reducer cleanup

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-// import Burger from '../../components/Burger/Burger';
-// import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 // import Modal from '../../components/UI/Modal/Modal';
-// import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import SingleRow from './SingleRow';
