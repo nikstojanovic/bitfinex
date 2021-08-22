d01c271fd07e435a9a7d2ec7a7e6d83c591a4b8c
Nikola Stojanovic <nikstojanovic84@gmail.com>
2021-08-22 17:48:17 +0200

    styling tweaks

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -14,10 +14,8 @@ class Modal extends Component {
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
                 <div
                     className="Modal"
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
+                    style={{opacity: this.props.show ? '1' : '0'}}
+                >
                     {this.props.children}
                 </div>
             </>
